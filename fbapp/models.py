b5f07e41fe49e42936d9601b8756aad4e22b37b7
YanBerdin <yandevformation@gmail.com>
2025-05-27 00:57:58 +0200

    feat: implement render_template
    - Implement render_template
    - Define Gender enum for content categorization
    - Implement database initialization function
    - Add seed data with sample content for testing

--- fbapp/models.py
+++ fbapp/models.py
@@ -1,22 +1,38 @@
 from flask_sqlalchemy import SQLAlchemy
 import logging as lg
 
+import enum
+
+class Gender(enum.Enum):
+    female = 0
+    male = 1
+    other = 2
+
 # Create database connection object
 db = SQLAlchemy()
 
 class Content(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    #L'appel à db.Column crée une nouvelle colonne dans la table
+    id = db.Column(db.Integer, primary_key=True) 
     description = db.Column(db.String(200), nullable=False)
-    gender = db.Column(db.Integer(), nullable=False)
+    gender = db.Column(db.Enum(Gender), nullable=False)
 
+    # La méthode __init__ construit l'instance
+    # elle est appelée lors de la création d'un nouvel objet Content.
     def __init__(self, description, gender):
         self.description = description
         self.gender = gender
+    # Content is a model representing a piece of content in the database.
+
+
 
 def init_db():
     db.drop_all()
     db.create_all()
-    db.session.add(Content("THIS IS SPARTAAAAAAA!!!", 1))
-    db.session.add(Content("What's your favorite scary movie?", 0))
+    # db.session.add(Content("THIS IS SPARTAAAAAAA!!!", 1))
+    # db.session.add(Content("What's your favorite scary movie?", 0))
+    db.session.add(Content("THIS IS SPARTAAAAAAA!!!", Gender['male']))
+    db.session.add(Content("What's your favorite scary movie?", Gender['female']))
+    db.session.add(Content('Personne ne te faisait pitié, ni sur le moment, ni après, on était absolument sans défense devant toi.', Gender['male']))
     db.session.commit()
     lg.warning('Database initialized!')
--- fbapp/views.py
+++ fbapp/views.py
@@ -1,5 +1,6 @@
 from flask import Flask
 import sys, os
+from flask import Flask, render_template # Import Flask and render_template for rendering HTML templates
 
 # Ajoutez le répertoire parent au path pour importer config
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
@@ -9,17 +10,12 @@ app = Flask(__name__)
 app.config.from_object(config)
 
 @app.route('/')
+@app.route('/index/')
 def index():
-    return "Hello world !"
+    # return "Hello world !"
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run()
-# This is a simple Flask application that returns "Hello world !" when accessed at the root URL.
-# To run this application, save it as views.py and execute it with Python.
-# Make sure you have Flask installed in your Python environment.
-# You can install Flask using pip:
-# pip install Flask
-# To run the application, use the command:
-# python views.py
-# Ensure that the Flask application is running by executing this script.
-# You can then access the application in your web browser at http://
+
+
